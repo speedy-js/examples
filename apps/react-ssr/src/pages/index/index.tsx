70bdc64ca3173d7a97377b25782f715474835039
林成璋 <linchengzhang@bytedance.com>
2022-02-25 14:55:41 +0800

    feat: update react-ssr template

---
+++ apps/react-ssr/src/pages/index/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// @ts-expect-error
+import logo from '../../assets/logo.svg';
+import './index.css';
+
+const App: React.FC = ({ repos = [] }) => {
+  React.useEffect(() => {
+    console.log('useEffect called');
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>pages/index/index.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://speedy.web.bytedance.net/ssr/introduction.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            SSR Docs
+          </a>
+        </p>
+        <p>
+          <Link className="App-link" to="/demo">
+            SSR Demo
+          </Link>
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
