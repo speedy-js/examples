a347108349f003122998ea3c3d6559b9b1b74e57
hardfist <yangjianzju@gmail.com>
2022-03-04 18:00:52 +0800

    chore: add output examples

---
+++ playground/output-html/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <p>Hello React</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById('root')
+)
---
+++ playground/output-mpa/src/first/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <p>First Page</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById('root')
+)
