78c1f088343f96a10de7f0da3e2a5317ad26700b
hardfist <yangjianzju@gmail.com>
2022-03-04 11:54:51 +0800

    chore: add playground

---
+++ playground/asset/README.md
@@ -0,0 +1,3 @@
+# Speedy Examples: Hello React
+
+This example shows how to configure a basic React project.
---
+++ playground/asset/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+function App() {
+  return (
+  <div>
+    <p>Hello World</p>
+  </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById('root')
+)
