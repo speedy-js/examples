533a1899f5d8ceaba253c137c84013d2a67015d7
hardfist <yangjianzju@gmail.com>
2022-03-09 17:16:25 +0800

    chore: add schema

---
+++ playground/schema/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function App() {
+  return (
+  <div>
+    <p>Hello World</p>
+  </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById('root')
+)
