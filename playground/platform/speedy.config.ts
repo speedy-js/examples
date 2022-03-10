923db7192dd083023bb9b281be9fcb28a5b88b56
hardfist <yangjianzju@gmail.com>
2022-03-10 11:56:58 +0800

    chore: add platform

---
+++ playground/platform/speedy.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig }  from '@speedy-js/speedy-core';
+import {nodePolyfillPlugin } from '@speedy-js/speedy-plugin-node-polyfill'
+export = defineConfig([{
+  input: {
+    node: './src/index.ts'
+  },
+  output: {
+    filename: 'node'
+  },
+  platform: 'node'
+}, {
+  input: {
+    browser: './src/index.ts'
+  },
+  output: {
+    filename: 'browser'
+  },
+  platform: 'browser',
+  plugins: [nodePolyfillPlugin({})]
+}])
---
+++ playground/platform/src/index.ts
@@ -0,0 +1,4 @@
+import assert from 'assert';
+
+
+assert(Math.random() > 0.5, 'not equal')
