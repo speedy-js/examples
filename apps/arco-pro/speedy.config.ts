81807501ec5ef575cf805d618d67b4e607bb472a
hardfist <yangjianzju@gmail.com>
2022-03-27 16:13:30 +0800

    chore: add local && esbuild dev for arco-pro

--- apps/arco-pro/speedy.config.ts
+++ apps/arco-pro/speedy.config.ts
@@ -1,12 +1,11 @@
 import {defineConfig} from '@speedy-js/speedy-core';
 import {svgrPlugin} from '@speedy-js/speedy-plugin-svgr'
-import {babelPlugin} from '@speedy-js/speedy-plugin-babel';
-import {unplugin} from '@speedy-js/unplugin';
 
 export = defineConfig({
   mode: 'development',
   input: {index: './src/index.tsx'},
   html: {},
+  sourceMap:false,
   plugins: [
     svgrPlugin({
       template: (
