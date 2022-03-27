494a64c118b8b7e45ee2fa956dba0440505e56cf
hardfist <yangjianzju@gmail.com>
2022-03-27 16:13:30 +0800

    chore: add arco build for speedy esbuild

---
+++ apps/arco-pro/speedy.config.js
@@ -0,0 +1,37 @@
+const { svgrPlugin } = require('@speedy-js/speedy-plugin-svgr');
+const { defineConfig } = require('@speedy-js/speedy-core');
+const os = require('os');
+const path = require('path');
+module.exports = defineConfig({
+  mode: 'development',
+  input: { index: './src/index.tsx' },
+  html: {},
+  profile:true,
+  sourceMap:false,
+  plugins: [
+    {
+      name: 'local-esbuild',
+      apply(compiler) {
+        compiler.hooks.esbuild.tap('esbuild', () => {
+          const homeDir = os.homedir();
+          const scriptPath = path.resolve(homeDir, 'github/speedybuild/scripts/esbuild.js');
+          const esbuild = require(scriptPath).installForTests();
+          return esbuild;
+        });
+      },
+    },
+    svgrPlugin({
+      template: ({ imports, interfaces, componentName, props, jsx }, { tpl }) => {
+        return tpl`${imports}
+    ${interfaces}
+
+    function ${componentName}(${props}) {
+      return ${jsx};
+    }
+    
+    export default ${componentName};
+      `;
+      },
+    }),
+  ],
+});
--- apps/arco-pro/speedy.config.ts
+++ apps/arco-pro/speedy.config.ts
@@ -1,12 +1,12 @@
 import {defineConfig} from '@speedy-js/speedy-core';
 import {svgrPlugin} from '@speedy-js/speedy-plugin-svgr'
-import {babelPlugin} from '@speedy-js/speedy-plugin-babel';
-import {unplugin} from '@speedy-js/unplugin';
 
 export = defineConfig({
   mode: 'development',
   input: {index: './src/index.tsx'},
   html: {},
+  profile: true,
+  sourceMap:false,
   plugins: [
     svgrPlugin({
       template: (
